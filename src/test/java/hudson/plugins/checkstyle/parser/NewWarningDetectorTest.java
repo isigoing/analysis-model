1652609b01848f4ba38dcb1914b9edca63ee5c4d
Ulli Hafner <ullrich.hafner@gmail.com>
2014-09-27 23:52:13 +0200

    Added another test that works.
    Here the new line is inserted at the beginning of the file.

--- src/test/java/hudson/plugins/checkstyle/parser/NewWarningDetectorTest.java
+++ src/test/java/hudson/plugins/checkstyle/parser/NewWarningDetectorTest.java
@@ -21,26 +21,43 @@ import hudson.plugins.analysis.util.model.FileAnnotation;
  */
 public class NewWarningDetectorTest {
     /**
-     * Verifies that the insertion of a new line will not mark a warning as new.
+     * Verifies that the insertion of a new line above the warning fails.
      */
     @Test
-    public void testInsertLine() {
+    public void testInsertLineAboveWarning() {
         FileAnnotation beforeWarning = readWarning("before/InsertLine.xml");
         FileAnnotation afterWarning = readWarning("after/InsertLine.xml");
 
         verifyWarning(beforeWarning, "Javadoc", 7, "InsertLine.java");
         verifyWarning(afterWarning, "Javadoc", 8, "InsertLine.java");
 
-        int beforeCode = createHashCode("before/InsertLine.java");
-        int afterCode = createHashCode("after/InsertLine.java");
+        int beforeCode = createHashCode("before/InsertLine.java", 7);
+        int afterCode = createHashCode("after/InsertLine.java", 8);
 
         assertEquals("Hash codes do not match: ", beforeCode, afterCode);
     }
 
-    private int createHashCode(final String fileName) {
+    /**
+     * Verifies that the insertion of a new line before the package declaration does not change the hash code.
+     */
+    @Test
+    public void testInsertLineBeforePackage() {
+        FileAnnotation beforeWarning = readWarning("before/InsertLine.xml");
+        FileAnnotation afterWarning = readWarning("after/InsertLine2.xml");
+
+        verifyWarning(beforeWarning, "Javadoc", 7, "InsertLine.java");
+        verifyWarning(afterWarning, "Javadoc", 8, "InsertLine.java");
+
+        int beforeCode = createHashCode("before/InsertLine.java", 7);
+        int afterCode = createHashCode("after/InsertLine2.java", 8);
+
+        assertEquals("Hash codes do not match: ", beforeCode, afterCode);
+    }
+
+    private int createHashCode(final String fileName, final int line) {
         try {
             ContextHashCode hashCode = new ContextHashCode();
-            return hashCode.create(read(fileName), "UTF-8", 7);
+            return hashCode.create(read(fileName), "UTF-8", line);
         }
         catch (IOException e) {
             throw new RuntimeException("Can't read Java file " + fileName, e);
