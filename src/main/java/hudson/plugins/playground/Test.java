2be152f44d86063fbc337e867dff64417ef9eb35
chr42 <moestl@hm.edu>
2014-10-11 17:58:56 +0200

    Added class only for try out sth

---
+++ src/main/java/hudson/plugins/playground/Test.java
@@ -0,0 +1,16 @@
+package hudson.plugins.playground;
+
+/**
+ * Klasse zum reinen Ausprobieren
+ *
+ * @author <a href="mailto:moestl@hm.edu">Christian M&ouml;stl</a>
+ */
+public class Test {
+    /**
+     * main-Method.
+     * @param args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Test");
+    }
+}
