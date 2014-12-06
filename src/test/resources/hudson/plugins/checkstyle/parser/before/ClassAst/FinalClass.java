b88e8211552221442ca1795a66546021b2f25818
Christian Moestl <moestl@hm.edu>
2014-12-06 19:54:59 +0100

    Added Test-classes in checkstyle.parser.after.ClassAst and checkstyle.parser.before.ClassAst etc.

---
+++ src/test/resources/hudson/plugins/checkstyle/parser/before/ClassAst/FinalClass.java
@@ -0,0 +1,15 @@
+package edu.hm.hafner;
+
+/**
+ * Useless class - only for test-cases.
+ *
+ * @author Christian M&ouml;stl
+ */
+public class FinalClass {
+
+    private int a;
+
+    private FinalClass() {
+
+    }
+}
